21877bf9ddc137dafeba4f081880ddf5b14180f0
afloey <103608489+afloey@users.noreply.github.com>
2022-05-04 18:01:05 -0700

    Add files via upload

---
+++ Jupyter-Notebook1/circleCircumfrence.py
@@ -0,0 +1,12 @@
+#circumfrence = 2 pi r
+
+from math import pi
+
+def circleCircumfrence():
+    radius = int(5)
+    circumfrence = 2 * pi * radius
+    print(circumfrence)
+    
+
+circleCircumfrence()
+
---
+++ Jupyter-Notebook1/squarefootage.py
@@ -0,0 +1,15 @@
+# length x width = area (ft)
+
+from aifc import Aifc_read
+
+
+def squarefootage():
+    area = (length * width)
+    print(area)
+
+length = 5
+width= 10
+
+squarefootage()
+
+
